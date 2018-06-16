f639615e7a85ef60126f840c643588411a544e1f
David Pollack <david@da3.net>
2018-06-16 12:13:44 +0200

    remove bytenet bce model from infer_all

--- infer_all_models.sh
+++ infer_all_models.sh
@@ -25,5 +25,4 @@ python -m infer --model-name attn --loss-criterion bce --batch-size 50 --max-len
 python -m infer --model-name attn --loss-criterion bce --batch-size 50 --max-len 48000 --load-model output/states/attn_bce_unbalanced_final_cached.pt
 python -m infer --model-name attn --loss-criterion crossentropy --batch-size 50 --max-len 48000 --load-model output/states/attn_crossentropy_balanced_final.pt
 # bytenet networks
-python -m infer --model-name bytenet --batch-size 10 --log-interval 20 --loss-criterion bce --max-len 57344 --load-model output/states/bytenet_bce_balanced_final.pt
 python -m infer --model-name bytenet --batch-size 10 --log-interval 20 --loss-criterion crossentropy --max-len 57344 --load-model output/states/bytenet_crossentropy_balanced_final.pt
