14231913fa4b6eb7e96645d16aca5e3b38a3a5a1
David Pollack <david@da3.net>
2018-06-16 12:10:40 +0200

    fix inference json filename

--- infer.py
+++ infer.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     assert config.load_model  # we need to load a model
     with torch.no_grad():
         infer()
-    cached = "cache" if config.use_cache else "nocache"
-    noise = "noise" if config.noises_dir else "nonoise"
+    cached = "nocache" if "nocache" in config.load_model else "cache"
+    noise = "nonoise" if "nonoise" in config.load_model else "noise"
     json_name = "output/inference_{}_{}_{}_{}_{}.json".format(config.model_name, config.loss_criterion, config.dataset, noise, cached)
     json.dump(config.infer_stats.tolist(), open(json_name, "w"))
     json_name = "output/output_{}_{}_{}_{}_{}.json".format(config.model_name, config.loss_criterion, config.dataset, noise, cached)
